48148775cc625afbff0545d9d65ca478adaac855
gnoronha42 <gabrielximenesnoronha@gmail.com>
2025-07-22 12:33:36 -0400

    feat:remove mideleware

--- index.js
+++ index.js
@@ -328,7 +328,7 @@ hr {
     
     // Configuração específica para Render
     const launchOptions = {
-      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/google-chrome-stable',
+
       headless: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
       defaultViewport: { width: 1280, height: 720 },
